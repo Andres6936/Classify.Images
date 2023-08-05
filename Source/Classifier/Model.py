2f95531e6fe30af6394dd7a2ea686029c04fd9cd
Andres6936 <andres6936@gmail.com>
2023-08-05 14:38:15 -0500

    Prepare Model for Predict

--- Source/Classifier/Model.py
+++ Source/Classifier/Model.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 from typing import AnyStr
-from Source.Classifier.LoaderImage import load_image, show_image
 
+from Source.Classifier.LoaderImage import load_image, show_image
 
 model_handle_map = {
     "efficientnetv2-s": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_s/classification/2",
@@ -112,88 +112,86 @@ model_image_size_map = {
 }
 
 
-def ClassifyImage() -> AnyStr:
-    image_size = 224
-    dynamic_size = False
+class Classifier:
+    def __init__(self):
+        labels_file = "https://storage.googleapis.com/bit_models/imagenet21k_wordnet_lemmas.txt"
+        # download labels and creates a maps
+        self.downloaded_file = tf.keras.utils.get_file("labels.txt", origin=labels_file)
+        self.classes = []
 
-    model_name = "efficientnetv2-l-21k"  # @param ['efficientnetv2-s', 'efficientnetv2-m', 'efficientnetv2-l', 'efficientnetv2-s-21k', 'efficientnetv2-m-21k', 'efficientnetv2-l-21k', 'efficientnetv2-xl-21k', 'efficientnetv2-b0-21k', 'efficientnetv2-b1-21k', 'efficientnetv2-b2-21k', 'efficientnetv2-b3-21k', 'efficientnetv2-s-21k-ft1k', 'efficientnetv2-m-21k-ft1k', 'efficientnetv2-l-21k-ft1k', 'efficientnetv2-xl-21k-ft1k', 'efficientnetv2-b0-21k-ft1k', 'efficientnetv2-b1-21k-ft1k', 'efficientnetv2-b2-21k-ft1k', 'efficientnetv2-b3-21k-ft1k', 'efficientnetv2-b0', 'efficientnetv2-b1', 'efficientnetv2-b2', 'efficientnetv2-b3', 'efficientnet_b0', 'efficientnet_b1', 'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5', 'efficientnet_b6', 'efficientnet_b7', 'bit_s-r50x1', 'inception_v3', 'inception_resnet_v2', 'resnet_v1_50', 'resnet_v1_101', 'resnet_v1_152', 'resnet_v2_50', 'resnet_v2_101', 'resnet_v2_152', 'nasnet_large', 'nasnet_mobile', 'pnasnet_large', 'mobilenet_v2_100_224', 'mobilenet_v2_130_224', 'mobilenet_v2_140_224', 'mobilenet_v3_small_100_224', 'mobilenet_v3_small_075_224', 'mobilenet_v3_large_100_224', 'mobilenet_v3_large_075_224']
+        with open(self.downloaded_file) as file:
+            labels = file.readlines()
+            self.classes = [line.strip() for line in labels]
 
-    model_handle = model_handle_map[model_name]
-
-    print(f"Selected model: {model_name} : {model_handle}")
-
-    max_dynamic_size = 512
-    if model_name in model_image_size_map:
-        image_size = model_image_size_map[model_name]
+    def ClassifyImage(self) -> AnyStr:
+        image_size = 224
         dynamic_size = False
-        print(f"Images will be converted to {image_size}x{image_size}")
-    else:
-        dynamic_size = True
-        print(f"Images will be capped to a max size of {max_dynamic_size}x{max_dynamic_size}")
 
-    labels_file = "https://storage.googleapis.com/bit_models/imagenet21k_wordnet_lemmas.txt"
+        model_name = "efficientnetv2-l-21k"  # @param ['efficientnetv2-s', 'efficientnetv2-m', 'efficientnetv2-l', 'efficientnetv2-s-21k', 'efficientnetv2-m-21k', 'efficientnetv2-l-21k', 'efficientnetv2-xl-21k', 'efficientnetv2-b0-21k', 'efficientnetv2-b1-21k', 'efficientnetv2-b2-21k', 'efficientnetv2-b3-21k', 'efficientnetv2-s-21k-ft1k', 'efficientnetv2-m-21k-ft1k', 'efficientnetv2-l-21k-ft1k', 'efficientnetv2-xl-21k-ft1k', 'efficientnetv2-b0-21k-ft1k', 'efficientnetv2-b1-21k-ft1k', 'efficientnetv2-b2-21k-ft1k', 'efficientnetv2-b3-21k-ft1k', 'efficientnetv2-b0', 'efficientnetv2-b1', 'efficientnetv2-b2', 'efficientnetv2-b3', 'efficientnet_b0', 'efficientnet_b1', 'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5', 'efficientnet_b6', 'efficientnet_b7', 'bit_s-r50x1', 'inception_v3', 'inception_resnet_v2', 'resnet_v1_50', 'resnet_v1_101', 'resnet_v1_152', 'resnet_v2_50', 'resnet_v2_101', 'resnet_v2_152', 'nasnet_large', 'nasnet_mobile', 'pnasnet_large', 'mobilenet_v2_100_224', 'mobilenet_v2_130_224', 'mobilenet_v2_140_224', 'mobilenet_v3_small_100_224', 'mobilenet_v3_small_075_224', 'mobilenet_v3_large_100_224', 'mobilenet_v3_large_075_224']
+        model_handle = model_handle_map[model_name]
 
-    # download labels and creates a maps
-    downloaded_file = tf.keras.utils.get_file("labels.txt", origin=labels_file)
+        print(f"Selected model: {model_name} : {model_handle}")
 
-    classes = []
+        max_dynamic_size = 512
+        if model_name in model_image_size_map:
+            image_size = model_image_size_map[model_name]
+            dynamic_size = False
+            print(f"Images will be converted to {image_size}x{image_size}")
+        else:
+            dynamic_size = True
+            print(f"Images will be capped to a max size of {max_dynamic_size}x{max_dynamic_size}")
 
-    with open(downloaded_file) as f:
-        labels = f.readlines()
-        classes = [l.strip() for l in labels]
+        image_name = "piano"  # @param ['tiger', 'bus', 'car', 'cat', 'dog', 'apple', 'banana', 'turtle', 'flamingo', 'piano', 'honeycomb', 'teapot']
+        images_for_test_map = {
+            "tiger": "https://upload.wikimedia.org/wikipedia/commons/b/b0/Bengal_tiger_%28Panthera_tigris_tigris%29_female_3_crop.jpg",
+            # by Charles James Sharp, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons
+            "bus": "https://upload.wikimedia.org/wikipedia/commons/6/63/LT_471_%28LTZ_1471%29_Arriva_London_New_Routemaster_%2819522859218%29.jpg",
+            # by Martin49 from London, England, CC BY 2.0 <https://creativecommons.org/licenses/by/2.0>, via Wikimedia Commons
+            "car": "https://upload.wikimedia.org/wikipedia/commons/4/49/2013-2016_Toyota_Corolla_%28ZRE172R%29_SX_sedan_%282018-09-17%29_01.jpg",
+            # by EurovisionNim, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons
+            "cat": "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg",
+            # by Alvesgaspar, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
+            "dog": "https://upload.wikimedia.org/wikipedia/commons/archive/a/a9/20090914031557%21Saluki_dog_breed.jpg",
+            # by Craig Pemberton, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
+            "apple": "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg",
+            # by Abhijit Tembhekar from Mumbai, India, CC BY 2.0 <https://creativecommons.org/licenses/by/2.0>, via Wikimedia Commons
+            "banana": "https://upload.wikimedia.org/wikipedia/commons/1/1c/Bananas_white_background.jpg",
+            # by fir0002  flagstaffotos [at] gmail.com       Canon 20D + Tamron 28-75mm f/2.8, GFDL 1.2 <http://www.gnu.org/licenses/old-licenses/fdl-1.2.html>, via Wikimedia Commons
+            "turtle": "https://upload.wikimedia.org/wikipedia/commons/8/80/Turtle_golfina_escobilla_oaxaca_mexico_claudio_giovenzana_2010.jpg",
+            # by Claudio Giovenzana, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
+            "flamingo": "https://upload.wikimedia.org/wikipedia/commons/b/b8/James_Flamingos_MC.jpg",
+            # by Christian Mehlführer, User:Chmehl, CC BY 3.0 <https://creativecommons.org/licenses/by/3.0>, via Wikimedia Commons
+            "piano": "https://upload.wikimedia.org/wikipedia/commons/d/da/Steinway_%26_Sons_upright_piano%2C_model_K-132%2C_manufactured_at_Steinway%27s_factory_in_Hamburg%2C_Germany.png",
+            # by "Photo: © Copyright Steinway & Sons", CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
+            "honeycomb": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Honey_comb.jpg",
+            # by Merdal, CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons
+            "teapot": "https://upload.wikimedia.org/wikipedia/commons/4/44/Black_tea_pot_cropped.jpg",
+            # by Mendhak, CC BY-SA 2.0 <https://creativecommons.org/licenses/by-sa/2.0>, via Wikimedia Commons
+        }
 
-    image_name = "piano"  # @param ['tiger', 'bus', 'car', 'cat', 'dog', 'apple', 'banana', 'turtle', 'flamingo', 'piano', 'honeycomb', 'teapot']
+        img_url = images_for_test_map[image_name]
+        image, original_image = load_image(img_url, image_size, dynamic_size, max_dynamic_size)
+        show_image(image, 'Scaled image')
 
-    images_for_test_map = {
-        "tiger": "https://upload.wikimedia.org/wikipedia/commons/b/b0/Bengal_tiger_%28Panthera_tigris_tigris%29_female_3_crop.jpg",
-        # by Charles James Sharp, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons
-        "bus": "https://upload.wikimedia.org/wikipedia/commons/6/63/LT_471_%28LTZ_1471%29_Arriva_London_New_Routemaster_%2819522859218%29.jpg",
-        # by Martin49 from London, England, CC BY 2.0 <https://creativecommons.org/licenses/by/2.0>, via Wikimedia Commons
-        "car": "https://upload.wikimedia.org/wikipedia/commons/4/49/2013-2016_Toyota_Corolla_%28ZRE172R%29_SX_sedan_%282018-09-17%29_01.jpg",
-        # by EurovisionNim, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons
-        "cat": "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg",
-        # by Alvesgaspar, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
-        "dog": "https://upload.wikimedia.org/wikipedia/commons/archive/a/a9/20090914031557%21Saluki_dog_breed.jpg",
-        # by Craig Pemberton, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
-        "apple": "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg",
-        # by Abhijit Tembhekar from Mumbai, India, CC BY 2.0 <https://creativecommons.org/licenses/by/2.0>, via Wikimedia Commons
-        "banana": "https://upload.wikimedia.org/wikipedia/commons/1/1c/Bananas_white_background.jpg",
-        # by fir0002  flagstaffotos [at] gmail.com       Canon 20D + Tamron 28-75mm f/2.8, GFDL 1.2 <http://www.gnu.org/licenses/old-licenses/fdl-1.2.html>, via Wikimedia Commons
-        "turtle": "https://upload.wikimedia.org/wikipedia/commons/8/80/Turtle_golfina_escobilla_oaxaca_mexico_claudio_giovenzana_2010.jpg",
-        # by Claudio Giovenzana, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
-        "flamingo": "https://upload.wikimedia.org/wikipedia/commons/b/b8/James_Flamingos_MC.jpg",
-        # by Christian Mehlführer, User:Chmehl, CC BY 3.0 <https://creativecommons.org/licenses/by/3.0>, via Wikimedia Commons
-        "piano": "https://upload.wikimedia.org/wikipedia/commons/d/da/Steinway_%26_Sons_upright_piano%2C_model_K-132%2C_manufactured_at_Steinway%27s_factory_in_Hamburg%2C_Germany.png",
-        # by "Photo: © Copyright Steinway & Sons", CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons
-        "honeycomb": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Honey_comb.jpg",
-        # by Merdal, CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons
-        "teapot": "https://upload.wikimedia.org/wikipedia/commons/4/44/Black_tea_pot_cropped.jpg",
-        # by Mendhak, CC BY-SA 2.0 <https://creativecommons.org/licenses/by-sa/2.0>, via Wikimedia Commons
-    }
+        classifier = hub.load(model_handle)
 
-    img_url = images_for_test_map[image_name]
-    image, original_image = load_image(img_url, image_size, dynamic_size, max_dynamic_size)
-    show_image(image, 'Scaled image')
+        input_shape = image.shape
+        warmup_input = tf.random.uniform(input_shape, 0, 1.0)
+        warmup_logits = classifier(warmup_input).numpy()
 
-    classifier = hub.load(model_handle)
+        # Run model on image
+        probabilities = tf.nn.softmax(classifier(image)).numpy()
 
-    input_shape = image.shape
-    warmup_input = tf.random.uniform(input_shape, 0, 1.0)
-    warmup_logits = classifier(warmup_input).numpy()
+        top_5 = tf.argsort(probabilities, axis=-1, direction="DESCENDING")[0][:5].numpy()
+        np_classes = np.array(self.classes)
 
-    # Run model on image
-    probabilities = tf.nn.softmax(classifier(image)).numpy()
+        # Some models include an additional 'background' class in the predictions, so
+        # we must account for this when reading the class labels.
+        includes_background_class = probabilities.shape[1] == 1001
 
-    top_5 = tf.argsort(probabilities, axis=-1, direction="DESCENDING")[0][:5].numpy()
-    np_classes = np.array(classes)
+        for i, item in enumerate(top_5):
+            class_index = item if includes_background_class else item + 1
+            line = f'({i + 1}) {class_index:4} - {self.classes[class_index]}: {probabilities[0][top_5][i]}'
+            print(line)
 
-    # Some models include an additional 'background' class in the predictions, so
-    # we must account for this when reading the class labels.
-    includes_background_class = probabilities.shape[1] == 1001
-
-    for i, item in enumerate(top_5):
-        class_index = item if includes_background_class else item + 1
-        line = f'({i + 1}) {class_index:4} - {classes[class_index]}: {probabilities[0][top_5][i]}'
-        print(line)
-
-    show_image(image, '')
+        show_image(image, '')
--- __main__.py
+++ __main__.py
@@ -1,12 +1,13 @@
 import sqlite3
 
 from Source.Classifier.LoaderFiles import GetListFilesRecursivelyAt
-from Source.Classifier.Model import ClassifyImage
+from Source.Classifier.Model import Classifier
 
 connection = sqlite3.connect("Images.sqlite")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE Images(Name TEXT PRIMARY KEY, Label TEXT)")
 imagesToClassify = GetListFilesRecursivelyAt("Pre-Trainer/**/*.webp")
+classifier = Classifier()
 
 for imageClassify in imagesToClassify:
-    label = ClassifyImage()
+    label = classifier.ClassifyImage()
