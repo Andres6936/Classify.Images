915e7f63d36862d8958c6678da970c80655c2a06
Andres6936 <andres6936@gmail.com>
2023-08-05 15:14:01 -0500

    Classify Image

--- __main__.py
+++ __main__.py
@@ -6,11 +6,12 @@ from Source.Classifier.Model import Classifier
 connection = sqlite3.connect("Images.sqlite")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS Images(Name TEXT PRIMARY KEY, Label TEXT, Probability REAL)")
-imagesToClassify = GetListFilesRecursivelyAt("Pre-Trainer/**/*.webp")
+images = GetListFilesRecursivelyAt("Pre-Trainer/**/*.webp")
 classifier = Classifier()
 
-for imageClassify in imagesToClassify:
+for imageClassify in images:
     prediction = classifier.ClassifyImage(imageClassify)
     cursor.execute(
         "INSERT INTO Images VALUES (?, ?, ?)",
         (imageClassify, prediction['Label'], prediction['Probability']))
+    connection.commit()
