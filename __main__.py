631785e843daa4b4a94bbb21dcc29155c657313d
Andres6936 <andres6936@gmail.com>
2023-08-17 09:30:24 -0500

    Catch Any Error

--- __main__.py
+++ __main__.py
@@ -9,10 +9,17 @@ from Source.Classifier.Model import Classifier
 
 
 def ImageAlreadyClassified(imagePath: AnyStr, cursor: Cursor):
-    response = cursor.execute("SELECT COUNT(1) FROM Images WHERE Name = ?", imagePath)
-    count = response.fetchone()
-    return True if count > 0 else False
+    """
+    Returns:
+        Return True if the image already classified, False otherwise.
 
+    Note:
+        it is assumed that any image name already in the database
+        has been classified.
+    """
+    response = cursor.execute(f"SELECT COUNT(1) FROM Images WHERE Name = '{imagePath}'")
+    count = response.fetchone()[0]
+    return True if count > 0 else False
 
 
 if __name__ == '__main__':
@@ -25,6 +32,7 @@ if __name__ == '__main__':
     connection = sqlite3.connect("Images.sqlite")
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS Images(Name TEXT PRIMARY KEY, Label TEXT, Probability REAL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS Errors(Name TEXT PRINARY KEY, Cause TEXT)")
     images: list[AnyStr] = GetListFilesRecursivelyAt("Pre-Trainer/**/*.webp")
     classifier = Classifier()
 
@@ -34,10 +42,15 @@ if __name__ == '__main__':
             logging.info(f"Image already classified, skipping image")
             continue
 
-        prediction = classifier.ClassifyImage(imageClassify)
-        cursor.execute(
-            "INSERT INTO Images VALUES (?, ?, ?)",
-            (imageClassify, prediction['Label'], prediction['Probability'].item()))
-        connection.commit()
-        logging.info(
-            f"Inserted register in database with values ({prediction['Label']}, {prediction['Probability'].item()})")
+        try:
+            prediction = classifier.ClassifyImage(imageClassify)
+            cursor.execute(
+                "INSERT INTO Images VALUES (?, ?, ?)",
+                (imageClassify, prediction['Label'], prediction['Probability'].item()))
+            logging.info(
+                f"Inserted register in database with values ({prediction['Label']}, {prediction['Probability'].item()})")
+        except Exception as e:
+            logging.warning(f"Error classifying image: {imageClassify}, saving information of error")
+            cursor.execute("INSERT INTO Errors VALUES (?, ?)", (imageClassify, str(e)))
+        finally:
+            connection.commit()
